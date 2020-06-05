2d201ec5746674560b2bfa128f2a3ff3d20c09fd
Quirin <quirin.ecker@bajupa.com>
2020-06-06 01:16:13 +0200

    repositioned the client.end statement now after the query

--- src/controller/ConfigRepository.ts
+++ src/controller/ConfigRepository.ts
@@ -16,10 +16,9 @@ export class ConfigRepository extends Repository<ServerConfig> {
                 (err, res) => {
                     if (res.rows.length < 1) reject('ServerConfig not found')
                     else resolve(new ServerConfig(res.rows[0].guildid, res.rows[0].prefix))
+                    DataBase.client.end();
                 }
             )
-
-            DataBase.client.end();
         })
     }
 
