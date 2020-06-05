6b973e24e4966c31d01b3951b132439225d4772f
Quirin <quirin.ecker@bajupa.com>
2020-06-05 18:45:34 +0200

    logs the object of the line for test purposes

--- src/commands/Settings.ts
+++ src/commands/Settings.ts
@@ -1,5 +1,7 @@
 import {Command} from "./Command";
 import {Message} from "eris";
+import {DataBase} from "../controller/DataBase";
+import {ServerConfig} from "../ServerConfig";
 
 export class Settings extends Command {
 
@@ -7,13 +9,15 @@ export class Settings extends Command {
         super('settings', 2, 3);
     }
 
-    protected run(msg: Message, args: Array<string>): void {
+    protected async run(msg: Message, args: Array<string>): Promise<void> {
         switch (args[0]) {
             case 'prefix':
                 const prefix = args[1];
+                const serverConfig: ServerConfig = await DataBase.configRepository.get(1);
+                console.log(serverConfig)
                 break;
             case 'role':
-                msg.channel.createMessage('changing the access role (Not jet implemented)');
+                await msg.channel.createMessage('changing the access role (Not jet implemented)');
                 break;
             default:
                 throw Error("Invalid Settings Property")
