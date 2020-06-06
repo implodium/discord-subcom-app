bf9c41b9cb8680206267c05ea2f7a1b3cd965d4c
Quirin <quirin.ecker@bajupa.com>
2020-06-06 18:11:31 +0200

    implemented update

--- src/controller/ConfigRepository.ts
+++ src/controller/ConfigRepository.ts
@@ -1,5 +1,6 @@
 import {Repository} from "./Repository";
 import {ServerConfig} from "../ServerConfig";
+import {DataBase} from "./DataBase";
 
 export class ConfigRepository extends Repository<ServerConfig> {
     async delete(id: number): Promise<void> {
@@ -15,7 +16,12 @@ export class ConfigRepository extends Repository<ServerConfig> {
     }
 
     async update(object: ServerConfig): Promise<number> {
-        return Promise.resolve(0);
+        const result = await DataBase.query(
+            "UPDATE config SET prefix = $2 WHERE guildid = $1",
+            [object.guildId, object.prefix]
+        );
+
+        return result.rowCount == 1 ? result.rowCount : -1;
     }
 
 }
