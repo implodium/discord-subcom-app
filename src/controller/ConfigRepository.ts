44f19dd49b87480b499a5f3f9e9d155a37fc18a1
Quirin <quirin.ecker@bajupa.com>
2020-06-06 17:14:21 +0200

    reseted the ConfigRepository.ts

--- src/controller/ConfigRepository.ts
+++ src/controller/ConfigRepository.ts
@@ -1,51 +1,20 @@
 import {Repository} from "./Repository";
 import {ServerConfig} from "../ServerConfig";
-import {DataBase} from "./DataBase";
 
 export class ConfigRepository extends Repository<ServerConfig> {
     async delete(id: number): Promise<void> {
         return Promise.resolve(undefined);
     }
 
-    async get(id: number): Promise<ServerConfig> {
-        await DataBase.client.connect()
+    async insert(object: ServerConfig): Promise<number> {
+        return Promise.resolve(0);
+    }
 
-        return new Promise((resolve, reject) => {
-            DataBase.client.query(
-                'SELECT * FROM config WHERE guildid = $1',
-                [id],
-                (err, res) => {
-                    if (res.rows.length < 1) reject('ServerConfig not found')
-                    else resolve(new ServerConfig(res.rows[0].guildid, res.rows[0].prefix))
-                    DataBase.client.end();
-                }
-            )
-        })
+    async get(id: number): Promise<ServerConfig> {
+        return Promise.resolve(new ServerConfig(1, ";"));
     }
 
     async update(object: ServerConfig): Promise<number> {
-        await DataBase.client.connect();
-
-        return new Promise((resolve, reject) => {
-            DataBase.client.query(
-                'UPDATE CONFIG SET prefix = $1 WHERE guildid = $2',
-                [object.prefix, object.guildId],
-                (err, res) => {
-                    if (err) {
-                        reject(err.stack)
-                    } else if (res.rowCount == 0) {
-                        reject("ServerConfig doesn't exist")
-                    } else {
-                        resolve(res.rowCount)
-                    }
-
-                    DataBase.client.end();
-                }
-            )
-        })
-    }
-
-    async insert(object: ServerConfig): Promise<number> {
         return Promise.resolve(0);
     }
 
