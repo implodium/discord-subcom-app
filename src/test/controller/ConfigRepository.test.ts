a46f14d7adc91ddf7cb4ff23ee118dac21195cb5
Quirin <quirin.ecker@bajupa.com>
2020-06-06 17:20:41 +0200

    implemented beforeEach for inserting testdata

--- src/test/controller/ConfigRepository.test.ts
+++ src/test/controller/ConfigRepository.test.ts
@@ -1,4 +1,17 @@
+import {DataBase} from "../../controller/DataBase";
+
 jest.setTimeout(30000);
 
-it('update test',  async () => {
+
+beforeEach(async(done) => {
+    await DataBase.query('DELETE FROM config where guildid = -1')
+    await DataBase.query(
+        'INSERT INTO config (guildid, prefix) values ($1, $2)',
+        [-1, ';']
+    )
+    done();
+})
+
+test('update',  async (done) => {
+    done();
 })
