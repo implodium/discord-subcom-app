abb1759e68593862841271f4fb8256e0024c493a
Quirin <quirin.ecker@bajupa.com>
2020-06-06 17:15:11 +0200

    unintentionally commited demo.ts

--- src/test/controller/demo.ts
+++
@@ -1,9 +0,0 @@
-import {DataBase} from "../../controller/DataBase";
-const func = async () => {
-    await DataBase.client.connect();
-    console.log(await DataBase.client.query('SELECT * FROM config'));
-}
-
-
-func()
-    .catch(console.log)
