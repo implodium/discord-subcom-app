bd9f804fdcf2a9a773c59f9022a0240f0eb1f7f3
Quirin <quirin.ecker@bajupa.com>
2020-06-10 12:14:10 +0200

    it is now possible to change the prefix through a discord command

--- src/commands/Settings.ts
+++ src/commands/Settings.ts
@@ -1,7 +1,8 @@
 import {Command} from "./Command";
-import {Message, TextChannel} from "eris";
+import {CommandClient, Message, TextChannel} from "eris";
 import {DataBase} from "../controller/DataBase";
 import {ServerConfig} from "../ServerConfig";
+import {SubComBot} from "../SubComBot";
 
 export class Settings extends Command {
 
@@ -12,14 +13,22 @@ export class Settings extends Command {
     protected async run(msg: Message, args: Array<string>): Promise<void> {
         switch (args[0]) {
             case 'prefix':
-                if (args.length == 1) {
-                    if (msg.channel instanceof TextChannel)  {
-                        const id: number = parseInt(msg.channel.guild.id);
-                        const serverConfig: ServerConfig = await DataBase.configRepository.get(id);
+                if (msg.channel instanceof TextChannel) {
+                    const id: string = msg.channel.guild.id
+                    const serverConfig: ServerConfig = await DataBase.configRepository.get(id);
+
+                    if (args.length == 1) {
                         await msg.channel.createMessage('**Prefix: **' + serverConfig.prefix);
-                    } else {
-                        throw new Error("This feature is only supported in text channels")
+                    } else if (args.length == 2) {
+                        const prefix = args[1];
+                        await msg.channel.createMessage(`Prefix was changed from ${serverConfig.prefix} to ${prefix}`)
+                        serverConfig.prefix = prefix;
+                        console.log(serverConfig)
+                        await DataBase.configRepository.update(serverConfig)
+                        SubComBot.instance.bot.registerGuildPrefix(serverConfig.guildId, `${serverConfig.prefix}subcom `);
                     }
+                } else {
+                    throw new Error("This feature is only supported in text channels")
                 }
                 break;
             case 'role':
