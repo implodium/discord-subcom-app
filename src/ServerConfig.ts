7552a3059c022d21be529f438b51018004364443
Quirin <quirin.ecker@bajupa.com>
2020-06-10 23:28:10 +0200

    added a permissions list to the ServerConfig

--- src/ServerConfig.ts
+++ src/ServerConfig.ts
@@ -1,9 +1,13 @@
+import {Permission} from "./model/Permission";
+
 export class ServerConfig {
     public prefix: string;
     public guildId: string;
+    public permissions: Array<Permission>;
 
     constructor(guildId: string, prefix: string) {
         this.guildId = guildId;
         this.prefix = prefix;
+        this.permissions = [];
     }
 }
