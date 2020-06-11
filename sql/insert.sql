9a870d8e1ff3db40b4097237166a9e56750ba1c8
Quirin <quirin.ecker@bajupa.com>
2020-06-11 02:53:39 +0200

    added some test inerts

--- sql/insert.sql
+++ sql/insert.sql
@@ -1,1 +1,4 @@
 insert into guild_config(guildId, prefix) values (-1, ';');
+insert into guild_config(guildId, prefix) values (-3, ';');
+insert into permission(roleid, count, guildid) values (-1, 1, -3);
+insert into permission(roleid, count, guildid) values (-2, 2, -3);
