e2601aad0b058cddc2dab69cac470e15f7c9faba
Quirin <quirin.ecker@bajupa.com>
2020-06-12 19:35:01 +0200

    now use tobeTruthy instead of not undefined

--- src/test/controller/PermissionRepository.test.ts
+++ src/test/controller/PermissionRepository.test.ts
@@ -13,8 +13,8 @@ beforeEach(async(done) => {
 test('get all', async done => {
     const permissions: Map<string, Permission> = await DataBase.permissionRepository.getAll('-3');
     expect(permissions.size).toBe(2);
-    expect(permissions.get('-1')).not.toBeUndefined();
-    expect(permissions.get('-2')).not.toBeUndefined();
+    expect(permissions.get('-1')).toBeTruthy();
+    expect(permissions.get('-2')).toBeTruthy();
     expect(permissions.get('-15')).toBeUndefined();
     expect((permissions.get('-1') as Permission).count).toBe(1);
     expect((permissions.get('-2') as Permission).count).toBe(2);
