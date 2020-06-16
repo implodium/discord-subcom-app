858d615a97439d74c4ad4032bf059e340950654d
Quirin <quirin.ecker@bajupa.com>
2020-06-16 11:43:56 +0200

    implemented Member class

--- src/Main/model/Member.ts
+++ src/Main/model/Member.ts
@@ -1,3 +1,9 @@
 export class Member {
+    public readonly id: string;
+    public countLeft: number;
 
+    constructor(id: string, countLeft: number) {
+        this.id = id;
+        this.countLeft = countLeft;
+    }
 }
