b3d6146c3f3002dbba4d52e13103e5332acab38f
Quirin <quirin.ecker@bajupa.com>
2020-06-21 20:32:01 +0200

    added test structure

---
+++ src/test/model/Subcom.test.ts
@@ -0,0 +1,15 @@
+import {SubCom} from "../../Main/model/SubCom";
+
+let subcom: SubCom;
+
+beforeEach(() => {
+    subcom = new SubCom('testName');
+})
+
+test('add Member', () => {
+
+})
+
+test('remove Member', () => {
+
+})
