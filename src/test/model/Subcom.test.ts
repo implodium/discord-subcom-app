0bda132f29e79cf80ac07c599055ec50cfa53f7c
Quirin <quirin.ecker@bajupa.com>
2020-06-21 20:53:51 +0200

    test implemented

--- src/test/model/Subcom.test.ts
+++ src/test/model/Subcom.test.ts
@@ -7,9 +7,15 @@ beforeEach(() => {
 })
 
 test('add Member', () => {
-
+    expect(subcom.members.length).toBe(0)
+    subcom.addMember('something')
+    expect(subcom.members[0]).toBe('something')
+    expect(subcom.members.length).toBe(1)
 })
 
 test('remove Member', () => {
-
+    subcom.members.push('something')
+    expect(subcom.members.length).toBe(1)
+    subcom.removeMember('something')
+    expect(subcom.members.length).toBe(0)
 })
