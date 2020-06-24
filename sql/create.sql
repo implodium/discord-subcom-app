3ecdede2e9132510e76052d1e8f675a2ea9aa096
Quirin <quirin.ecker@bajupa.com>
2020-06-24 20:39:48 +0200

    edited the sql files with two new tables

--- sql/create.sql
+++ sql/create.sql
@@ -17,4 +17,17 @@ CREATE TABLE MEMBER (
     id varchar(18),
     count int,
     CONSTRAINT PK_MEMBER PRIMARY KEY (id)
-)
+);
+
+CREATE TABLE SUBCOM(
+    categoryId varchar(18),
+    name varchar(20),
+    ownerId varchar(18),
+    CONSTRAINT PK_SUBCOM PRIMARY KEY (categoryId)
+);
+
+CREATE TABLE SUBCOM_MEMBER_ASSOZIATION(
+    subcomId varchar(18),
+    memberId varchar(18),
+    CONSTRAINT PK_SUBCOM_MEMBER_ASSOCIATION PRIMARY KEY (subcomId, memberId)
+);
