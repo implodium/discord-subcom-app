560218f85421186a04d03c99f7b95539f561d968
Quirin <quirin.ecker@bajupa.com>
2020-06-25 08:08:19 +0200

    added some inserts to the subcom table

--- sql/insert.sql
+++ sql/insert.sql
@@ -4,3 +4,5 @@ insert into permission(roleid, count, guildid) values (-1, 1, -3);
 insert into permission(roleid, count, guildid) values (-2, 2, -3);
 insert into member(id, count) values (-1, 3);
 insert into member(id, count) values (-2, 5);
+insert into subcom(categoryid, name, ownerid) values (-1, 'some name', -1);
+insert into subcom(categoryid, name, ownerid) values (-2, 'some name', -2);
