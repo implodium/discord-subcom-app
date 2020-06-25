6c499435678e356df9a71e768ebe8581f9f99807
Quirin <quirin.ecker@bajupa.com>
2020-06-25 08:16:58 +0200

    created insert repository test for subcoms

---
+++ src/test/controller/SubComRepository.test.ts
@@ -0,0 +1,31 @@
+import {DataBase} from "../../Main/controller/DataBase";
+import {QueryResult} from "pg";
+import {SubCom} from "../../Main/model/SubCom";
+
+beforeEach(async done => {
+    await DataBase.queryFile('./sql/drop.sql')
+        .catch(console.log)
+    await DataBase.queryFile('./sql/create.sql')
+        .catch(console.log)
+    await DataBase.queryFile('./sql/insert.sql')
+        .catch(console.log)
+    done();
+})
+
+test('insert', async done => {
+    await DataBase.subComRepository.insert(new SubCom(
+        '-3',
+        'some name',
+        '-3'
+    ));
+
+    const result: QueryResult = await DataBase.query(
+        'SELECT * FROM member WHERE id = $1',
+        ['-3']
+    );
+
+    expect(result.rows[0].name).toBe('some name');
+    expect(result.rows[0].categoryid).toBe(-3);
+    expect(result.rows[0].ownerId).toBe(-3)
+    done();
+})
