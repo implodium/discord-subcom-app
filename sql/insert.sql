bbed525fe393153c309fff38d418982cc83de58f
Quirin <quirin.ecker@bajupa.com>
2020-06-25 12:54:57 +0200

    added test row

--- sql/insert.sql
+++ sql/insert.sql
@@ -6,3 +6,4 @@ insert into member(id, count) values (-1, 3);
 insert into member(id, count) values (-2, 5);
 insert into subcom(categoryid, name, ownerid) values (-1, 'some name 1', -1);
 insert into subcom(categoryid, name, ownerid) values (-2, 'some name 2', -2);
+insert into subcom(categoryid, name, ownerid) values (-4, 'some name 1', -1);
