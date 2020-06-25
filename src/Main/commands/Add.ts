f3db1b2ace00a5fd68c6b1b35067a8aa227960e6
Quirin <quirin.ecker@bajupa.com>
2020-06-25 17:35:53 +0200

    added argument validation

--- src/Main/commands/Add.ts
+++ src/Main/commands/Add.ts
@@ -9,7 +9,13 @@ export class Add extends Command {
     }
 
     protected run(msg: Message, args: Array<string>): void {
-        console.log(args[0])
-        console.log(args[1])
+        if (Add.isValid(args)) {
+            console.log(args[0])
+            console.log(args[1])
+        } else throw Error('Wrong Arrangement of Arguments')
+    }
+
+    private static isValid(args: Array<string>): boolean {
+        return args[0].includes('@') && args[1].includes('#');
     }
 }
