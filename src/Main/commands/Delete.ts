6e2d42df30e2b445ac6761af352d2c9efed647dd
Quirin <quirin.ecker@bajupa.com>
2020-06-28 14:50:37 +0200

    deleting subcoms works on the discord client

--- src/Main/commands/Delete.ts
+++ src/Main/commands/Delete.ts
@@ -1,13 +1,31 @@
 import {Command} from "./Command";
-import {Message} from "eris";
+import {CategoryChannel, Message, TextChannel, User} from "eris";
+import {DataBase} from "../controller/DataBase";
+import {SubCom} from "../model/SubCom";
 
 export class Delete extends Command{
     constructor() {
         super('delete', 1, 1);
     }
 
-    protected run(msg: Message, args: Array<string>): void {
-        console.log(msg.author.id)
-        console.log(args[0])
+    protected async run(msg: Message, args: Array<string>): Promise<void> {
+        const channel: TextChannel = msg.channel as TextChannel;
+        const handleId: string = args[0].replace(/[^0-9]/g, '');
+        const handle: TextChannel = channel.guild.channels.get(handleId) as TextChannel;
+        const category: CategoryChannel = channel.guild.channels.get(handle.parentID as string) as CategoryChannel;
+        const subcom: SubCom = await DataBase.subComRepository.get(handle.parentID as string)
+
+        if (Delete.isPermitted(msg.author, subcom)) {
+            category.channels.forEach(channel => {
+                channel.delete().catch(console.log)
+            })
+
+            category.delete()
+                .catch(console.log)
+        }
+    }
+
+    private static isPermitted(author: User, subcom: SubCom) {
+        return subcom.ownerId === author.id;
     }
 }
